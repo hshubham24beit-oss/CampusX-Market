9d458b85a00cc0776c105135bedfe835b55123c0
Shubham <hshubham24beit@student.mes.ac.in>
2026-07-26 14:37:06 +0530

    Configure MongoDB Atlas and database connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,13 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/database.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
